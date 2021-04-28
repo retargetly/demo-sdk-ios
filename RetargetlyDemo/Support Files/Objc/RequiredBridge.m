b70ed78bd745d114e7464c59af7e979aa43b5730
José Valderrama <josevalderrama18@gmail.com>
2021-04-28 18:26:18 +0200

    * Update to iOS 14.5

---
+++ RetargetlyDemo/Support Files/Objc/RequiredBridge.m
@@ -0,0 +1,9 @@
+//
+//  RequiredBridge.m
+//  RetargetlyDemo
+//
+//  Created by José Valderrama on 28/04/2021.
+//  Copyright © 2021 Retargetly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
